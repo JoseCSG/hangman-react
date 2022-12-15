2e7d7a1f8ff2717d64c65a6329d3ac521a8d913e
José Carlos <a01174050@tec.mx>
2022-12-15 17:13:43 -0600

    added some things, didnt work tho

--- hangman-react/src/index.js
+++ hangman-react/src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const words = ["comida", "perro", "alegria"];
+function underline(props)
+{
+	return(
+		<p className='underscore'>{props.value}</p>
+	)
+}
+class wordHidden extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			word: Array(words[2].length).fill(null),
+		}
+	}
+	renderUnderscore(i)
+	{
+		return <underline 
+				value = {this.state.word[i]}
+				/>;
+	}
+	render()
+	{
+		return (
+			<div className='word'>
+				{this.renderUnderscore(1)}
+			</div>
+		)
+	}
+}
+class Game extends React.Component {
+	render() 
+  	{
+		return (
+			<div className='Game'>
+				<div>
+					<h1>Hangman</h1>
+				</div>
+				<div className='gameWord'>
+					<wordHidden/>
+				</div>
+			</div>
+		)
+  	}
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Game />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
